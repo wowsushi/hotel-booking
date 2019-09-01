cca148de4653cd27b0c7b6067fe5f94fe4aca442
Betty <s9654025@gmail.com>
2019-09-01 16:06:40 +0800

    Updates

---
+++ precache-manifest.8bf85dedf626fc25cbeb41e6cadbd5ff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79c26bac9528af3a1a5d438d00f20eec",
+    "url": "/hotel-booking/index.html"
+  },
+  {
+    "revision": "8c20fd1a900b332f6508",
+    "url": "/hotel-booking/static/css/main.ec26a4ae.chunk.css"
+  },
+  {
+    "revision": "6b289893cc672e6d4e25",
+    "url": "/hotel-booking/static/js/2.2c4868cc.chunk.js"
+  },
+  {
+    "revision": "8c20fd1a900b332f6508",
+    "url": "/hotel-booking/static/js/main.422934e6.chunk.js"
+  },
+  {
+    "revision": "99d62e5765110ed6ba17",
+    "url": "/hotel-booking/static/js/runtime~main.2346735c.js"
+  }
+]);
